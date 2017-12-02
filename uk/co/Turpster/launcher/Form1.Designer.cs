20fc21423ed1d754620e1d2a89120709420c27a9
Turpster <www-reece--10-1@hotmail.com>
2017-12-02 16:40:51 +0000

    Introduced Windows App Form C# form

---
+++ uk/co/Turpster/launcher/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace launcher
+{
+    partial class winLauncher
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.webUpdates = new System.Windows.Forms.WebBrowser();
+            this.pictureGameNameImage = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureGameNameImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(485, 464);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(145, 20);
+            this.txtUsername.TabIndex = 0;
+            this.txtUsername.TextChanged += new System.EventHandler(this.txtUsername_TextChanged);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(485, 490);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(145, 20);
+            this.txtPassword.TabIndex = 1;
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(424, 467);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(55, 13);
+            this.lblUsername.TabIndex = 2;
+            this.lblUsername.Text = "Username";
+            this.lblUsername.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(426, 493);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(53, 13);
+            this.lblPassword.TabIndex = 3;
+            this.lblPassword.Text = "Password";
+            this.lblPassword.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Font = new System.Drawing.Font("Alegreya Sans SC", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlay.Location = new System.Drawing.Point(636, 464);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(130, 46);
+            this.btnPlay.TabIndex = 4;
+            this.btnPlay.Text = "play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            // 
+            // webUpdates
+            // 
+            this.webUpdates.Location = new System.Drawing.Point(12, 12);
+            this.webUpdates.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webUpdates.Name = "webUpdates";
+            this.webUpdates.Size = new System.Drawing.Size(754, 446);
+            this.webUpdates.TabIndex = 5;
+            this.webUpdates.Url = new System.Uri("https://www.google.co.uk/", System.UriKind.Absolute);
+            this.webUpdates.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // pictureGameNameImage
+            // 
+            this.pictureGameNameImage.Location = new System.Drawing.Point(12, 467);
+            this.pictureGameNameImage.Name = "pictureGameNameImage";
+            this.pictureGameNameImage.Size = new System.Drawing.Size(193, 50);
+            this.pictureGameNameImage.TabIndex = 6;
+            this.pictureGameNameImage.TabStop = false;
+            // 
+            // winLauncher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 522);
+            this.Controls.Add(this.pictureGameNameImage);
+            this.Controls.Add(this.webUpdates);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.Name = "winLauncher";
+            this.Text = "Turpster";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureGameNameImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.WebBrowser webUpdates;
+        private System.Windows.Forms.PictureBox pictureGameNameImage;
+    }
+}
+
---
+++ uk/co/Turpster/launcher/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace launcher
+{
+    public partial class winLauncher : Form
+    {
+        public winLauncher()
+        {
+            InitializeComponent();
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void txtUsername_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
