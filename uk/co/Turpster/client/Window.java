d333edc10d5fbf3b3a8c1f883fbe94816efde7e6
Turpster <www-reece--10-1@hotmail.com>
2017-12-05 06:08:53 +0000

    FIxed Window Size Bug

--- uk/co/Turpster/client/Window.java
+++ uk/co/Turpster/client/Window.java
@@ -11,7 +11,7 @@ public class Window extends JFrame
 		super("War Gauge");
 		int width = 800, height = 600;
 		
-		this.setSize(width, height);
+		this.setSize(width + 16, height + 39);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
 		WarGauge game = new WarGauge(args, width, height);
