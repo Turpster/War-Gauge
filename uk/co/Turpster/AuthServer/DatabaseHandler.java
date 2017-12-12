cd097b2a1687d27b9ea4e873f22311561158eb3d
Turpster <dicky_056@hotmail.co.uk>
2017-12-12 15:53:55 +0000

    changed database handler to a thread

--- uk/co/Turpster/AuthServer/DatabaseHandler.java
+++ uk/co/Turpster/AuthServer/DatabaseHandler.java
@@ -4,9 +4,12 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DatabaseHandler 
+public class DatabaseHandler implements Runnable
 {
 	Connection connection;
+	
+	private Thread thread;
+	private boolean running = false;
 	
 	private String
 	serverAddress,
@@ -28,6 +31,38 @@ public class DatabaseHandler
 		this.username = username;
 		this.password = password;
 		this.port = port;
+	}
+	
+	public synchronized void start()
+	{
+		thread = new Thread(this, "Database-Handler");
+		thread.start();
+		
+		running = true;
+	}
+	
+	public synchronized void stop()
+	{
+		try 
+		{
+			thread.join();
+		} 
+		catch (InterruptedException e) 
+		{
+			Main.getLogger().log(Logger.WARNING, "Error occured when trying to join connection thread");
+			stop();
+			
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void run()
+	{
+		while (running)
+		{
+		
+		}
 	}
 	
 	public void connect(String databasename) throws SQLException
